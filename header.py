98e77e5f455495ba555fe8b0f1717251fdb4a5d8
Sunu M S <75416351+sunums@users.noreply.github.com>
2022-11-29 17:40:48 +0000

    Delete header.py

--- header.py
+++
@@ -1,4 +0,0 @@
-def timesjobhtml():
-    url ='https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=AEROSPACE&txtLocation='
-    #return(requests.get(f'https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=aeospace&txtLocation=').text)
-    return(url)
